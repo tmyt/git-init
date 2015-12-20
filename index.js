5ab0e45939c2234e4cb761fcf895fc8d1356ee5d
yutaka <shigure@refy.net>
2015-12-20 12:58:05 +0000

    support template dir

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ module.exports = init
 
 // initialize a git repo
 // (str, fn) -> null
-function init (path, bare, cb) {
+function init (path, bare, template, cb) {
+  if (typeof template === 'function') {
+    cb = template;
+    template = '';
+  }
   if (typeof bare === 'function') {
     cb = bare
+    template = '';
     bare = false
   }
   if (typeof path === 'function') {
     cb = path
+    template = '';
     bare = false
     path = ''
   }
   path = path || ''
-  exec('git init -q ' + (bare ? '--bare ' : '') + path, cb)
+  exec('git init -q ' + (bare ? '--bare ' : '')  + (template !== '' ? '--template=' + template + ' ' : '') + path, cb)
 }
 
